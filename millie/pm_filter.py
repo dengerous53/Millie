cb2023164c4c236b2cf2baa3278c4760f580bc7d
Millie-bobby <120792111+Millie-bobby@users.noreply.github.com>
2022-12-25 20:40:51 +0100

    Update pm_filter.py

--- millie/pm_filter.py
+++ millie/pm_filter.py
@@ -935,7 +935,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "sourcedata":
         await message.reply_photo(
-            photo=random.choice(PICS),
+            photo="https://te.legra.ph/file/5b2ec8d541fee97e63037.jpg",
             caption=f"This boy's source is private if you want the source just contact developer \n\ndev 1: [Millie bobby brown](https://t.me/millie_rmvzz)\n\ndev2 : [Daddy ](https://t.me/renish_rgi)",
             disable_web_page_preview=True,
             reply_markup=reply_markup,
