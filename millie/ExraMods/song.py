818e005ba8ab0bef1e0fce0c594ce43a9dc8ba90
Rplay <88985252+Renishrplay@users.noreply.github.com>
2023-02-13 09:51:53 +0530

    Update song.py

--- millie/ExraMods/song.py
+++ millie/ExraMods/song.py
@@ -107,7 +107,7 @@ async def vsong(client, message: Message):
     )
     if not urlissed:
         await pablo.edit("Invalid Command Syntax Please Check help Menu To Know More!")
-    elif urlissed == noun:
+    elif urlissed == None:
         await pablo.edit("hi")
         return
 
