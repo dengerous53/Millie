ff3d747750bf87a66963f155c9c11686c9ef8dfb
Millie-bobby <120792111+Millie-bobby@users.noreply.github.com>
2023-02-25 23:56:46 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -103,10 +103,7 @@ async def get_poster(query, bulk=False, id=False, file=None):
     if not id:
         # https://t.me/GetTGLink/4183
         query = (query.strip()).lower()
-        title = queryT_FILES = []
-    T_SIZE = []
-    T_USERS = []
-    T_CHATS = []
+        title = query
         year = re.findall(r'[1-2]\d{3}$', query, re.IGNORECASE)
         if year:
             year = list_to_str(year[:1])
