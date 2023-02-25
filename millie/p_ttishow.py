fde858579326a82782a52fdf0ba5dfd6162bda4d
Rplay <88985252+Renishrplay@users.noreply.github.com>
2023-02-25 15:19:11 +0530

    Update p_ttishow.py

--- millie/p_ttishow.py
+++ millie/p_ttishow.py
@@ -193,7 +193,7 @@ async def re_paid_chat(bot, message):
 
 
 @Client.on_message(filters.command('stats'))
-def get_ststs(bot, message):
+async def get_ststs(bot, message):
     total_users = await db.total_users_count()
     totl_chats = await db.total_chat_count()
     files = await Media.count_documents()
