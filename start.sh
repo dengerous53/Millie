a1b21c33db22c1f92ec54884593e788931b7196c
Rplay <88985252+Renishrplay@users.noreply.github.com>
2023-02-25 14:24:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "copying your bot data ...."
-git clone https://github.com/Renishrplay/Millie-1.git /Millie
+git clone https://github.com/Renishrplay/Millie-1 /Millie
 cd /Millie
 echo "installing some packages..."
 pip3 install -U -r requirements.txt
