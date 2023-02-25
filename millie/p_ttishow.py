8b71e89c168c40d3c2bf52c1f71d80e7b57e24ce
Rplay <88985252+Renishrplay@users.noreply.github.com>
2023-02-25 23:17:08 +0530

    Update p_ttishow.py

--- millie/p_ttishow.py
+++ millie/p_ttishow.py
@@ -200,7 +200,7 @@ InlineKeyboardButton('𝚁𝙴𝙵𝚁𝙴𝚂𝙷', callback_data='statrfr')
 def statsmsg(client, message):
     message.reply_photo(
     photo=random.choice(PICS),
-    caption = script.STATUS_TXT.format(temp.T_FILES, temp.T_USERS, temp.T_CHATS, 536870912, temp.F_SIZE),
+    caption = script.STATUS_TXT.format(temp.T_FILES, temp.T_USERS, temp.T_CHATS, temp.T_SIZE, temp.F_SIZE),
     reply_markup = InlineKeyboardMarkup(STATSBTN)
     )
 
