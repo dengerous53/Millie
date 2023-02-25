9bb4d405f55b4e4fe48389bf4a67828d660dadd8
Millie-bobby <120792111+Millie-bobby@users.noreply.github.com>
2023-02-25 23:26:57 +0530

    Update millie.py

--- millie.py
+++ millie.py
@@ -5,7 +5,7 @@ from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from database.users_chats_db import db
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, LOG_CHANNEL, PORT
-from utils import temp
+from utils import get_size, temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
 from datetime import datetime
@@ -45,10 +45,17 @@ class Bot(Client):
         await super().start()
         await Media.ensure_indexes()
         me = await self.get_me()
+        monsize = await db.get_db_size()
+        free = 536870912 - monsize
         temp.ME = me.id
         temp.U_NAME = me.username
         temp.B_NAME = me.first_name
         temp.B_LINK = me.mention
+        temp.T_FILES = await Media.count_documents()
+        temp.U_SIZE = get_size(monsize)
+        temp.T_USERS = await db.total_users_count()
+        temp.T_CHATS = await db.total_chat_count()
+        temp.F_SIZE = get_size(free)
         self.username = '@' + me.username
         curr = datetime.now(timezone(TIMEZONE))
         date = curr.strftime('%d %B, %Y')
