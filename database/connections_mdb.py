b97dd3a0b2ad292a6b5b8a32e0a863a8c1349d8f
Rplay <88985252+Renishrplay@users.noreply.github.com>
2023-02-26 10:21:46 +0530

    Update connections_mdb.py

--- database/connections_mdb.py
+++ database/connections_mdb.py
@@ -1,6 +1,6 @@
 import pymongo
-
-from info import DATABASE_URI, DATABASE_NAME
+from pymongo import MongoClient
+from info import DATABASE_URI, DATABASE_NAME, LOGGER
 
 import logging
 logger = logging.getLogger(__name__)
@@ -10,6 +10,13 @@ myclient = pymongo.MongoClient(DATABASE_URI)
 mydb = myclient[DATABASE_NAME]
 mycol = mydb['CONNECTION']   
 
+
+try:
+    millie_db_client = MongoClient(DATABASE_URI)
+except PyMongoError as f:
+    LOGGER.error(f"Error in Mongodb: {f}")
+    exiter(1)
+millie_main_db = alita_db_client[DATABASE_NAME]
 
 async def add_connection(group_id, user_id):
     query = mycol.find_one(
@@ -133,3 +140,65 @@ async def delete_connection(user_id, group_id):
     except Exception as e:
         logger.exception(f'Some error occured! {e}', exc_info=True)
         return False
+
+class MongoDB:
+    """Class for interacting with Bot database."""
+
+    def __init__(self, collection) -> None:
+        self.collection = millie_main_db[collection]
+
+    # Insert one entry into collection
+    def insert_one(self, document):
+        result = self.collection.insert_one(document)
+        return repr(result.inserted_id)
+
+    # Find one entry from collection
+    def find_one(self, query):
+        result = self.collection.find_one(query)
+        if result:
+            return result
+        return False
+
+    # Find entries from collection
+    def find_all(self, query=None):
+        if query is None:
+            query = {}
+        return list(self.collection.find(query))
+
+    # Count entries from collection
+    def count(self, query=None):
+        if query is None:
+            query = {}
+        return self.collection.count_documents(query)
+
+    # Delete entry/entries from collection
+    def delete_one(self, query):
+        self.collection.delete_many(query)
+        return self.collection.count_documents({})
+
+    # Replace one entry in collection
+    def replace(self, query, new_data):
+        old = self.collection.find_one(query)
+        _id = old["_id"]
+        self.collection.replace_one({"_id": _id}, new_data)
+        new = self.collection.find_one({"_id": _id})
+        return old, new
+
+    # Update one entry from collection
+    def update(self, query, update):
+        result = self.collection.update_one(query, {"$set": update})
+        new_document = self.collection.find_one(query)
+        return result.modified_count, new_document
+
+    @staticmethod
+    def close():
+        return millie_db_client.close()
+
+
+def __connect_first():
+    _ = MongoDB("test")
+    LOGGER.info("Initialized Database!\n")
+
+
+__connect_first()
+
