2bb0cf76be8477dde10eac1979b15015d8611e2a
Rplay <88985252+Renishrplay@users.noreply.github.com>
2023-03-06 18:11:42 +0530

    Add files via upload

--- millie/group_manage/chatbot.py
+++ millie/group_manage/chatbot.py
@@ -1,94 +1,146 @@
-import requests
+from asyncio import gather, sleep
+from info import *
 from pyrogram import filters
 from pyrogram.types import Message
-from googletrans import Translator
-from info import *
-from millie.group_manage.tr_engine import tlang as language
-from millie.group_manage.database import chatb  
-from millie.helper.admin_check import admin_fliter as admin_filter
+from Python_ARQ import ARQ
+from aiohttp import ClientSession
+import logging
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
 
-
+ARQ_API_KEY = "XKCIMS-WNUNYK-BJFTHJ-YMMCKY-ARQ"
+ARQ_API_URL = "https://arq.hamker.in"
+USERBOT_PREFIX = USERBOT_PREFIX
+chatbot_group = 2
+aiohttpsession = ClientSession()
+active_chats_bot = []
+active_chats_ubot = []
 BOT_ID = int(BOT_TOKEN.split(":")[0])
+SUDOERS = ADMINS
+arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
 
-tr = Translator()
-CBOT = "chatbot"
-CBOTA = "millie"
-cbot = 2
+LOGGER.info("Gathering profile info")
+x = app.get_me()
+y = app2.get_me()
 
-def get_arg(message):
-    msg = message.text
-    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
-    split = msg[1:].replace("\n", " \n").split(" ")
-    if " ".join(split[1:]).strip() == "":
-        return ""
-    return " ".join(split[1:])
+BOT_USERNAME = x.username
+USERBOT_ID = y.id
+
+async def eor(msg: Message, **kwargs):
+    func = (
+        (msg.edit_text if msg.from_user.is_self else msg.reply)
+        if msg.from_user
+        else msg.reply
+    )
+    spec = getfullargspec(func.__wrapped__).args
+    return await func(**{k: v for k, v in kwargs.items() if k in spec})
 
 
-@app.on_message(filters.command("chatbot") & ~filters.private& admin_filter)
-@language
-async def cbots(client, message: Message, _):
-    group_id = str(message.chat.id)
+async def chat_bot_toggle(db, message: Message):
+    status = message.text.split(None, 1)[1].lower()
     chat_id = message.chat.id
-    user_id = message.from_user.id
-    user = await app.get_chat_member(group_id, user_id)
-    if not user.status == "creator" or user.status == "administrator":
-        return
-    if len(message.command) < 2:
-        return await message.reply_text(_["chatb1"])
-    status = message.text.split(None, 1)[1].strip()
-    status = status.lower()
-    args = get_arg(message)
-    sex = await message.reply_text(_["antil2"])
-    lower_args = args.lower()
-    if lower_args == "on":
-        chatb.insert_one({f"chatbot": group_id})
-    elif lower_args == "off":
-        chatb.delete_one({f"chatbot": group_id})
+    if status == "enable":
+        if chat_id not in db:
+            db.append(chat_id)
+            text = "Chatbot Enabled!"
+            return await eor(message, text=text)
+        await eor(message, text="ChatBot Is Already Enabled.")
+    elif status == "disable":
+        if chat_id in db:
+            db.remove(chat_id)
+            return await eor(message, text="Chatbot Disabled!")
+        await eor(message, text="ChatBot Is Already Disabled.")
     else:
-        return await sex.edit(_["chatb1"])
-    await sex.edit(f"✅ **Successfully** `{'Enabled' if lower_args=='on' else 'Disabled'}` ** Chat bot**")
+        await eor(message, text="**Usage:**\n/chatbot [ENABLE|DISABLE]")
 
-@app.on_message(filters.text & filters.reply & ~filters.bot & ~filters.via_bot & ~filters.forwarded & ~filters.private , group=cbot)
-async def szcbot(_, message: Message):
+
+# Enabled | Disable Chatbot
+
+
+@Client.on_message(filters.command("chatbot") & ~filters.edited)
+async def chatbot_status(_, message: Message):
+    if len(message.command) != 2:
+        return await eor(message, text="**Usage:**\n/chatbot [ENABLE|DISABLE]")
+    await chat_bot_toggle(active_chats_bot, message)
+
+
+async def lunaQuery(query: str, user_id: int):
+    luna = await arq.luna(query, user_id)
+    return luna.result
+
+
+async def type_and_send(message: Message):
     chat_id = message.chat.id
+    user_id = message.from_user.id if message.from_user else 0
+    query = message.text.strip()
+    await message._client.send_chat_action(chat_id, "typing")
+    response, _ = await gather(lunaQuery(query, user_id), sleep(3))
+    await message.reply_text(response)
+    await message._client.send_chat_action(chat_id, "cancel")
+
+
+@Client.on_message(
+    filters.text
+    & filters.reply
+    & ~filters.bot
+    & ~filters.via_bot
+    & ~filters.forwarded
+    & ~filters.edited,
+    group=chatbot_group,
+)
+async def chatbot_talk(_, message: Message):
+    if message.chat.id not in active_chats_bot:
+        return
     if not message.reply_to_message:
         return
     if not message.reply_to_message.from_user:
         return
     if message.reply_to_message.from_user.id != BOT_ID:
         return
-    if message.text[0] == "/":
-        return
-    chat = chatb.find_one({"chatbot":chat_id})   
-    if chat:
-        await app.send_chat_action(message.chat.id, "typing")
-        try:
-           lang = tr.translate(message.text).src
-           trtoen = (message.text if lang=="en" else tr.translate(message.text, dest="en").text).replace(" ", "%20")
-           text = trtoen.replace(" ", "%20") if len(message.text) < 2 else trtoen
-           affiliateplus = requests.get(f"https://api.affiliateplus.xyz/api/chatbot?message={text}&botname=Rose&ownername=@supunma&user={chat_id}")
-           textmsg = (affiliateplus.json()["message"])
-           if "Affiliate+" in textmsg:
-               textmsg = textmsg.replace("Affiliate+", "Rose")
-           if "Lebyy_Dev" in textmsg:
-               textmsg = textmsg.replace("Lebyy_Dev", "Supun Maduranga")
-           if "God Brando" in textmsg:
-               textmsg = textmsg.replace("God Brando", f"{message.from_user.first_name}")
-           if "seeker" in textmsg:
-               textmsg = textmsg.replace("seeker", f"wow")
-           msg = tr.translate(textmsg, src='en', dest=lang)
-           await message.reply_text(msg.text)
-        except Exception:
-           user_id = message.from_user.id
-           lang = tr.translate(message.text).src
-           trtoen = (message.text if lang=="en" else tr.translate(message.text, dest="en").text).replace(" ", "%20")
-           text = trtoen.replace(" ", "%20") if len(message.text) < 2 else trtoen
-           safeone = requests.get(f"https://api.safone.tech/chatbot?message={text}&bot_name=Rose&bot_master=Supun&user_id={user_id}")
-           textmsg = (safeone.json()["answer"])
-           if "Affiliate+" in textmsg:
-               textmsg = textmsg.replace("Affiliate+", "Rose")
-           if "[Safone]" in textmsg:
-               textmsg = textmsg.replace("[Safone]", "Supun Maduranga")
-           msg = tr.translate(textmsg, src='en', dest=lang)
-           await message.reply_text(msg.text)
+    await type_and_send(message)
 
+
+# FOR USERBOT
+
+
+@Client.on_message(
+    filters.command("chatbot", prefixes=USERBOT_PREFIX)
+    & ~filters.edited
+    & SUDOERS
+)
+async def chatbot_status_ubot(_, message: Message):
+    if len(message.text.split()) != 2:
+        return await eor(message, text="**Usage:**\n.chatbot [ENABLE|DISABLE]")
+    await chat_bot_toggle(active_chats_ubot, message)
+
+
+@Client.on_message(
+    ~filters.me & ~filters.private & filters.text & ~filters.edited,
+    group=chatbot_group,
+)
+async def chatbot_talk_ubot(_, message: Message):
+    if message.chat.id not in active_chats_ubot:
+        return
+    username = "@" + str(USERBOT_USERNAME)
+    if message.reply_to_message:
+        if not message.reply_to_message.from_user:
+            return
+        if (
+                message.reply_to_message.from_user.id != USERBOT_ID
+                and username not in message.text
+        ):
+            return
+    else:
+        if username not in message.text:
+            return
+    await type_and_send(message)
+
+
+@Client.on_message(
+    filters.text & filters.private & ~filters.me & ~filters.edited,
+    group=(chatbot_group + 1),
+)
+async def chatbot_talk_ubot_pm(_, message: Message):
+    if message.chat.id not in active_chats_ubot:
+        return
+    await type_and_send(message)
