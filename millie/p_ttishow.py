28b11b82e85710ace66a0f8d749dc2acfab3e08a
Rplay <88985252+Renishrplay@users.noreply.github.com>
2023-03-11 16:37:23 +0530

    Update p_ttishow.py

--- millie/p_ttishow.py
+++ millie/p_ttishow.py
@@ -375,7 +375,7 @@ async def list_users(bot, message):
 @Client.on_message(filters.command('pusers') & filters.user(ADMINS))
 async def plist_users(bot, message):
     sps = await message.reply('Getting List Of Users')
-    users = await db.get_paid_status()
+    users = prime_user
     out = "Users Saved In DB Are:\n\n"
     async for user in users:
         out += f"<a href=tg://user?id={user['id']}>{user['name']}</a>\n"
