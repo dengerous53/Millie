d35072e6eaef79f7ac0cff45ed60af838059c6c7
Rplay <88985252+Renishrplay@users.noreply.github.com>
2023-03-12 01:56:06 +0530

    Update song.py

--- millie/ExraMods/song.py
+++ millie/ExraMods/song.py
@@ -105,10 +105,6 @@ async def vsong(client, message: Message):
     pablo = await client.send_message(
         message.chat.id, f"**𝙵𝙸𝙽𝙳𝙸𝙽𝙶 𝚈𝙾𝚄𝚁 𝚅𝙸𝙳𝙴𝙾** `{urlissed}`"
     )
-    if not urlissed:
-        await pablo.edit("Invalid Command Syntax Please Check help Menu To Know More!")
-        return pablo.edit("hello")
-
     search = SearchVideos(f"{urlissed}", offset=1, mode="dict", max_results=1)
     mi = search.result()
     mio = mi["search_result"]
