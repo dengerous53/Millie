0c190f953e141c20ed8c7589de9695842d48a619
Rplay <88985252+Renishrplay@users.noreply.github.com>
2023-03-12 13:08:42 +0530

    Update filters_mdb.py

--- database/filters_mdb.py
+++ database/filters_mdb.py
@@ -32,7 +32,7 @@ async def find_filter(group_id, name):
     mycol = mydb[str(group_id)]
     
     query = mycol.find( {"text":name})
-    # query = mycol.find( { "$text": {"$search": name}})
+    query = mycol.find( { "$text": {"$search": name}})
     try:
         for file in query:
             reply_text = file['reply']
