deabc149f45f9e79fb61da0216539506e87a1825
Rplay <88985252+Renishrplay@users.noreply.github.com>
2023-03-17 10:36:09 +0530

    Update pm_filter.py

--- millie/pm_filter.py
+++ millie/pm_filter.py
@@ -1368,7 +1368,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 ],
                 [
                 InlineKeyboardButton(
-                    'ShortLink',
+                    'ShortLink', if ADMINS else await ("Your Not Authorizer ⚠️", show_alert=True)
                     callback_data=f'setgs#is_shortlink#{settings["is_shortlink"]}#{grp_id}',
                 ),
                 InlineKeyboardButton(
