d21169e9d15c48093a54740d9e548f754c3d25a7
Rplay <88985252+Renishrplay@users.noreply.github.com>
2023-03-18 09:12:54 +0530

    Update commands.py

--- millie/commands.py
+++ millie/commands.py
@@ -477,7 +477,7 @@ async def settings(client, message):
             ],
             [
                 InlineKeyboardButton(
-                    'ShortLink, ADMINS', else await query.answer("oKda", show_alert=True)
+                    if int(ShortLink)is in ADMINS, else await query.answer("oKda", show_alert=True)
                     callback_data=f'setgs#is_shortlink#{settings["is_shortlink"]}#{grp_id}',
                 ),
                 InlineKeyboardButton(
