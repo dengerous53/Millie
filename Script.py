856c19db94056090e12cdea5207b3d082098c396
Millie-bobby <120792111+Millie-bobby@users.noreply.github.com>
2023-03-18 18:42:35 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -2,7 +2,8 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}\n𝙷𝙴𝚁𝙴 𝙸𝚂 𝙼𝚈 𝙷𝙴𝙻𝙿 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
 
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴 : {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁 : <a href=https://t.me/millie_rmvzz>Millie Bobby brown</a>
+✯ 𝙳𝙰𝙳𝙳𝚈 : <a href=https://t.me/renish_rgi>❁✗❍═❰ RENISH ❱═❍✗❁</a>
+✯ 𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝚒𝚍 : <a href=https://t.me/Millie_support>Millie Support</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈 : 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 : 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴 : 𝙼𝙾𝙽𝙶𝙾-𝙳𝙱
