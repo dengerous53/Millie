dbecb4f7c7e22a74cbb73ea9e1c09d75602df00c
Rplay <88985252+Renishrplay@users.noreply.github.com>
2023-03-18 16:58:33 +0530

    Update pm_filter.py

--- millie/pm_filter.py
+++ millie/pm_filter.py
@@ -33,6 +33,20 @@ FILTER_MODE = {}
 G_MODE = {}
 
 
+@Client.on_message(filters.private & filters.text & filters.incoming)
+async def pm_text(bot, message):
+    content = message.text
+    user = message.from_user.first_name
+    user_id = message.from_user.id
+    if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
+    if user_id in ADMINS: return # ignore admins
+    await message.reply_text("<b>ᴊᴜsᴛ sᴇᴀʀᴄʜ ᴏɴ ɢʀᴏᴜᴘs ʙᴇᴄᴀᴜsᴇ ᴡᴇ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ!</b>")
+    await bot.send_message(
+        chat_id=LOG_CHANNEL,
+        text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
+    )
+
+
 @Client.on_message(filters.command('autofilter'))
 async def fil_mod(client, message): 
       mode_on = ["yes", "on", "true"]
