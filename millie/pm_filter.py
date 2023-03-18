a4f7f0680a821ea5fe8c8037f6d97b19ecbf9a6e
Rplay <88985252+Renishrplay@users.noreply.github.com>
2023-03-18 17:45:34 +0530

    Update pm_filter.py

--- millie/pm_filter.py
+++ millie/pm_filter.py
@@ -40,7 +40,7 @@ async def pm_text(bot, message):
     user_id = message.from_user.id
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
     if user_id in ADMINS: return # ignore admins
-    await message.reply_text("<b>ᴊᴜsᴛ sᴇᴀʀᴄʜ ᴏɴ ɢʀᴏᴜᴘs ʙᴇᴄᴀᴜsᴇ ᴡᴇ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ!</b>")
+    await message.reply_text("<b>welcome </b>")
     await bot.send_message(
         chat_id=LOG_CHANNEL,
         text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
