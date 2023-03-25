a643b71604d75354657653a5d62f46e8ed859d80
Rplay <88985252+Renishrplay@users.noreply.github.com>
2023-03-25 19:08:13 +0530

    Update pm_filter.py

--- millie/pm_filter.py
+++ millie/pm_filter.py
@@ -1020,7 +1020,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=f"<b>𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝗋 : {mention} ({reporter})\n\n𝖬𝖾𝗌𝗌𝖺𝗀𝖾 : {content}</b>",
-            text+=f"select an option!",
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
