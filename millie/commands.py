0bcb5315b7f36e9460eed117cce15df6f4cbeaa7
Millie-bobby <120792111+Millie-bobby@users.noreply.github.com>
2023-03-26 16:35:33 +0530

    Update commands.py

--- millie/commands.py
+++ millie/commands.py
@@ -509,7 +509,7 @@ async def handle_requests(bot, message):
     
     if success:
         btn = [[
-                InlineKeyboardButton("JOIN CHANNEL", url="https://telegram.me/millie_robot_update")
+                InlineKeyboardButton("🧧 VIEW STATUS 🧧", url="https://telegram.me/millie_robot_update")
                ]]
         await message.reply_text("<b>Your request has been added! Please wait for some time.</b>", reply_markup=InlineKeyboardMarkup(btn))
 
