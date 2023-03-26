544361c9c0c30446965a11999219754754e21e82
Millie-bobby <120792111+Millie-bobby@users.noreply.github.com>
2023-03-26 17:12:31 +0530

    Update pm_filter.py

--- millie/pm_filter.py
+++ millie/pm_filter.py
@@ -911,11 +911,11 @@ async def cb_handler(client: Client, query: CallbackQuery):
             await query.message.edit_reply_markup(reply_markup)
             await query.answer("Set to Unavailable !")
             try:
-                await client.send_message(chat_id=int(from_user), text=f"<b>Hey {user.mention}, Sorry Your requested movie is Rejected. So our moderators can't upload it.</b>", reply_markup=InlineKeyboardMarkup(btn2))
+                await client.send_message(chat_id=int(from_user), text=f"<b>Hey {user.mention}, Sorry your request has been rejected. Either you have not joined our group or you have been banned. So join the group first or talk to our Admin.</b>", reply_markup=InlineKeyboardMarkup(btn2))
             except UserIsBlocked:
-                await client.send_message(chat_id=int(SUPPORT_CHAT_ID), text=f"<b>Hey {user.mention}, Sorry Your requested movie is Rejected. So our moderators can't upload it.\n\nNote: This message is sent to this group because you've blocked the bot. To send this message to your PM, Must unblock the bot.</b>", reply_markup=InlineKeyboardMarkup(btn2))
+                await client.send_message(chat_id=int(SUPPORT_CHAT_ID), text=f"<b>Hey {user.mention}, Sorry your request has been rejected. Either you have not joined our group or you have been banned. So join the group first or talk to our Admin.\n\nNote: This message is sent to this group because you've blocked the bot. To send this message to your PM, Must unblock the bot.</b>", reply_markup=InlineKeyboardMarkup(btn2))
         else:
-            await query.answer("You don't have sufficiant rigts to do this !", show_alert=True)
+            await query.answer("CHACK In pm  !", show_alert=True)
 
     elif query.data.startswith("unavailable"):
         ident, from_user = query.data.split("#")
