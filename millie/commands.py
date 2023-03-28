511b1cb6d87a6cf0735d13754be73180b301226e
Rplay <88985252+Renishrplay@users.noreply.github.com>
2023-03-28 21:50:08 +0530

    Update commands.py

--- millie/commands.py
+++ millie/commands.py
@@ -206,7 +206,6 @@ async def start(client, message):
     elif data.split("-", 1)[0] == "verify":
         userid = data.split("-", 2)[1]
         token = data.split("-", 3)[2]
-        file_id = data.split("-", 4)[3] # Add this line to extract file_id
         if str(message.from_user.id) != str(userid):
             return await message.reply_text(
                 text="<b>Invalid link or Expired link !</b>",
@@ -214,26 +213,15 @@ async def start(client, message):
             )
         is_valid = await check_token(client, userid, token)
         if is_valid == True:
-            btn = [[
-                    InlineKeyboardButton("Verify", callback_data=f"{file_id}")
-                ]]
             await message.reply_text(
                 text=f"<b>Hey {message.from_user.mention}, You are successfully verified !\nNow you have unlimited access for all movies till today midnight.</b>",
-                file_id=message.get("file_id"),
-                protect_content=True,
-                reply_markup=InlineKeyboardMarkup(btn)
-                )
+                protect_content=True
+            )
+            await verify_user(client, userid, token)
         else:
-            try:
-                kk, file_id = message.command[1].split("_", 1)
-                pre = 'checksubp' if kk == 'filep' else 'checksub' 
-                btn.append([InlineKeyboardButton(" 🔄 Try Again", callback_data=f"{pre}#{file_id}")])
-            except (IndexError, ValueError):
-                btn.append([InlineKeyboardButton(" 🔄 Try Again", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
             return await message.reply_text(
                 text="<b>Invalid link or Expired link !</b>",
-                protect_content=True,
-                Buttons=[[InlineKeyboardButton("📢 𝚄𝙿𝙳𝙰𝚃𝙴𝚂 📢", url="https://t.me/millie_robot_update")]
+                protect_content=True
             )
 
     files_ = await get_file_details(file_id)           
