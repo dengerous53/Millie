d0f804f539a0d07142b5bfc25b4b02a56768ce6d
Rplay <88985252+Renishrplay@users.noreply.github.com>
2023-03-28 15:47:17 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -527,18 +527,23 @@ async def check_token(bot, userid, token):
     else:
         return False
 
-async def get_token(bot, userid, link):
+async def get_token(bot, userid, link, message, file_id):
     user = await bot.get_users(userid)
     try:
-                kk, file_id = message.command[1].split("_", 1)
-                pre = 'checksubp' if kk == 'filep' else file_id=f"{pre}#{file_id}"
+        kk, file_id = message.command[1].split("_", 1)
+        pre = 'checksubp' if kk == 'filep' else 'file'
+        file_id = f"{pre}#{file_id}"
+    except IndexError:
+        pass
+
     if not await db.is_user_exist(user.id):
         await db.add_user(user.id, user.first_name)
         await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(user.id, user.mention))
+
     token = ''.join(random.choices(string.ascii_letters + string.digits, k=7))
     TOKENS[user.id] = {token: False}
-    link = f"{link}verify-{user.id}-{token}-file_{file.id}"
-    shortened_verify_url = await get_verify_shorted_link(link)
+    link = f"{link}/verify-{user.id}-{token}-{file_id}"
+    shortened_verify_url = await get_verify_shortened_link(link)
     return str(shortened_verify_url)
 
 async def verify_user(bot, userid, token):
