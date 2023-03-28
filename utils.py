9a3b9818301f892c2d314b382b497b4ba113831e
Rplay <88985252+Renishrplay@users.noreply.github.com>
2023-03-28 15:13:33 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -529,12 +529,13 @@ async def check_token(bot, userid, token):
 
 async def get_token(bot, userid, link):
     user = await bot.get_users(userid)
+    file_id=msg.get("file_id"),
     if not await db.is_user_exist(user.id):
         await db.add_user(user.id, user.first_name)
         await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(user.id, user.mention))
     token = ''.join(random.choices(string.ascii_letters + string.digits, k=7))
     TOKENS[user.id] = {token: False}
-    link = f"{link}verify-{user.id}-{token}"
+    link = f"{link}verify-{user.id}-{token}-{file_id}"
     shortened_verify_url = await get_verify_shorted_link(link)
     return str(shortened_verify_url)
 
