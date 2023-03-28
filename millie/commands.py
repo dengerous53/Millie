29197dc33ca840f020edf47002da828ec527fd65
Rplay <88985252+Renishrplay@users.noreply.github.com>
2023-03-28 15:06:30 +0530

    Update commands.py

--- millie/commands.py
+++ millie/commands.py
@@ -215,7 +215,7 @@ async def start(client, message):
         is_valid = await check_token(client, userid, token)
         if is_valid == True:
             btn = [[
-                    InlineKeyboardButton("Verify", callback_data=f"{pre}#{file_id}")
+                    InlineKeyboardButton("Verify", callback_data=f"{file_id}")
                 ]]
             await message.reply_text(
                 text=f"<b>Hey {message.from_user.mention}, You are successfully verified !\nNow you have unlimited access for all movies till today midnight.</b>",
