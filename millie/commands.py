5bc571f7ce6d8cece1f8dd606efef80a9578d0df
Rplay <88985252+Renishrplay@users.noreply.github.com>
2023-03-29 10:35:18 +0530

    Update commands.py

--- millie/commands.py
+++ millie/commands.py
@@ -230,9 +230,10 @@ async def start(client, message):
     if not files_:
         pre, file_id = ((base64.urlsafe_b64decode(data + "=" * (-len(data) % 4))).decode("ascii")).split("_", 1)
         try:
-            if not await check_verification(client, message.from_user.id) and VERIFY == True:
+            file_id = message.command[1].split("_", 1)
+            if not await check_verification(client, message.from_user.id, file_id) and VERIFY == True:
                 btn = [[
-                    InlineKeyboardButton("Verify", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start="))
+                    InlineKeyboardButton("Verify", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start=", file_id))
                 ]]
                 await message.reply_text(
                     text="<b>You are not verified !\nKindly verify to continue !</b>",
