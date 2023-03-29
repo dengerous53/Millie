2c8e9921c50d66f64488800c9b71fbc1a3d2d231
Rplay <88985252+Renishrplay@users.noreply.github.com>
2023-03-29 10:39:40 +0530

    Update commands.py

--- millie/commands.py
+++ millie/commands.py
@@ -230,8 +230,7 @@ async def start(client, message):
     if not files_:
         pre, file_id = ((base64.urlsafe_b64decode(data + "=" * (-len(data) % 4))).decode("ascii")).split("_", 1)
         try:
-            file_id = message.command[1].split("_", 1)
-            if not await check_verification(client, message.from_user.id, file_id) and VERIFY == True:
+            if not await check_verification(client, message.from_user.id) and VERIFY == True:
                 btn = [[
                     InlineKeyboardButton("Verify", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start=", file_id))
                 ]]
