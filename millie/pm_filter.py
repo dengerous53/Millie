451f1c3fe54d23e42d1784e3a30d6225950a869e
Rplay <88985252+Renishrplay@users.noreply.github.com>
2023-03-30 13:54:40 +0530

    Update pm_filter.py

--- millie/pm_filter.py
+++ millie/pm_filter.py
@@ -1583,6 +1583,11 @@ async def auto_filter(client, msg, spoll=False):
 
 
 async def advantage_spell_chok(msg):
+    srh_msg = await msg.reply_text("<b>Lᴏᴀᴅɪɴɢ Yᴏᴜʀ Rᴇsᴜʟᴛs...🔎</b>")
+    mv_id = msg.id
+    mv_rqst = msg.text
+    reqstr1 = msg.from_user.id if msg.from_user else 0
+    settings = await get_settings(msg.chat.id)
     query = re.sub(
         r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)",
         "", msg.text, flags=re.IGNORECASE)  # plis contribute some common words
