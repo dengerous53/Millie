1a49540d873644cd021e15cc3f9035eac346f0d6
Millie-bobby <120792111+Millie-bobby@users.noreply.github.com>
2023-04-01 23:03:37 +0530

    Update afk.py

--- millie/group_manage/afk.py
+++ millie/group_manage/afk.py
@@ -49,53 +49,35 @@ async def going_afk(bot, message: Message):
             data = reasondb["data"]
             reasonafk = reasondb["reason"]
             seenago = get_readable_time((int(time.time() - timeafk)))
+            reply_text = f"**{message.from_user.first_name}** is back online and was away for {seenago}"
+            if reasonafk:
+                reply_text += f"\n\nReason: `{reasonafk}`"
             if afktype == "text":
-                return await message.reply_text(f"**{message.from_user.first_name}** is back online and was away for {seenago}",disable_web_page_preview=True)
-            if afktype == "text_reason":
-                return await message.reply_text(f"**{message.from_user.first_name}** is back online and was away for {seenago}\n\nReason: `{reasonafk}`",disable_web_page_preview=True,)
-            if afktype == "animation":
-                if str(reasonafk) == "None":
-                    return await message.reply_animation(data,caption=f"**{message.from_user.first_name}** is back online and was away for {seenago}")
-                else:
-                    return await message.reply_animation(data,caption=f"**{message.from_user.first_name}** is back online and was away for {seenago}\n\nReason: `{reasonafk}")
-            if afktype == "photo":
-                if str(reasonafk) == "None":
-                    return await message.reply_photo(photo=f"downloads/{user_id}.jpg",caption=f"**{message.from_user.first_name}** is back online and was away for {seenago}")
-                else:
-                    return await message.reply_photo(photo=f"downloads/{user_id}.jpg",caption=f"**{message.from_user.first_name}** is back online and was away for {seenago}\n\nReason: `{reasonafk}`")                
+                await message.reply_text(reply_text, disable_web_page_preview=True)
+            elif afktype == "animation":
+                await message.reply_animation(data, caption=reply_text)
+            elif afktype == "photo":
+                await message.reply_photo(photo=f"downloads/{user_id}.jpg", caption=reply_text)
         except Exception as e:
             print(e)
-            return await message.reply_text(f"**{message.from_user.first_name}** is back online",disable_web_page_preview=True)
-    if len(message.command) == 1 and not message.reply_to_message:
-        details = {"type": "text","time": time.time(),"data": None,"reason": None}
-    elif len(message.command) > 1 and not message.reply_to_message:
-        _reason = (message.text.split(None, 1)[1].strip())[:100]
-        details = {"type": "text_reason","time": time.time(),"data": None,"reason": _reason}
-    elif (len(message.command) == 1 and message.reply_to_message.animation):
-        _data = message.reply_to_message.animation.file_id
-        details = {"type": "animation","time": time.time(),"data": _data,"reason": None}
-    elif (len(message.command) > 1 and message.reply_to_message.animation):
-        _data = message.reply_to_message.animation.file_id
-        _reason = (message.text.split(None, 1)[1].strip())[:100]
-        details = {"type": "animation","time": time.time(),"data": _data,"reason": _reason}
-    elif len(message.command) == 1 and message.reply_to_message.photo:
-        await app.download_media(message.reply_to_message, file_name=f"{user_id}.jpg")
-        details = {"type": "photo","time": time.time(),"data": None,"reason": None}
-    elif len(message.command) > 1 and message.reply_to_message.photo:
-        await app.download_media(message.reply_to_message, file_name=f"{user_id}.jpg")
-        _reason = message.text.split(None, 1)[1].strip()
-            details = {"type": "photo","time": time.time(),"data": None,"reason": None}
-    elif (len(message.command) > 1 and message.reply_to_message.sticker):
-        _reason = (message.text.split(None, 1)[1].strip())[:100]
-        if message.reply_to_message.sticker.is_animated:
-            details = {"type": "text_reason","time": time.time(),"data": None,"reason": _reason}
-           else:
+            await message.reply_text(f"**{message.from_user.first_name}** is back online", disable_web_page_preview=True)
+    elif len(message.command) == 1 and not message.reply_to_message:
+        details = {"type": "text", "time": time.time(), "data": None, "reason": None}
+        await add_afk(user_id, details)
+        await message.reply_text(f"{message.from_user.first_name} is now afk!")
+    elif message.reply_to_message and (
+        message.reply_to_message.animation or message.reply_to_message.photo or message.reply_to_message.sticker
+    ):
+        if message.reply_to_message.animation:
+            _data = message.reply_to_message.animation.file_id
+            _type = "animation"
+        elif message.reply_to_message.photo:
+            _type = "photo"
             await app.download_media(message.reply_to_message, file_name=f"{user_id}.jpg")
-            details = {"type": "photo","time": time.time(),"data": None,"reason": None}
-    elif (len(message.command) > 1 and message.reply_to_message.sticker):
-        _reason = (message.text.split(None, 1)[1].strip())[:100]
-        if message.reply_to_message.sticker.is_animated:
-            details = {"type": "text_reason","time": time.time(),"data": 
+            _data = f"downloads/{user_id}.jpg"
+        elif message.reply_to_message.sticker:
+            if message.reply_to_message.sticker.is_animated:
+                _type = "text"
             else:
                 _type = "photo"
                 await app.download_media(message.reply_to_message, file_name=f"{user_id}.jpg")
@@ -103,12 +85,18 @@ async def going_afk(bot, message: Message):
         if len(message.command) > 1:
             _reason = (message.text.split(None, 1)[1].strip())[:100]
         else:
-            await app.download_media(message.reply_to_message, file_name=f"{user_id}.jpg")
-            details = {"type": "photo","time": time.time(),"data": None,"reason": _reason}
+            _reason = None
+        details = {"type": _type, "time": time.time(), "data": _data, "reason": _reason}
+        await add_afk(user_id, details)
+        await message.reply_text(f"{message.from_user.first_name} is now afk!")
+    elif len(message.command) > 1:
+        _reason = (message.text.split(None, 1)[1].strip())[:100]
+        details = {"type": "text_reason", "time": time.time(), "data": None, "reason": _reason}
+        await add_afk(user_id, details)
+        await message.reply_text(f"{message.from_user.first_name} is now afk!")
     else:
-        details = {"type": "text","time": time.time(),"data": None,"reason": None}
-    await add_afk(user_id, details)
-    await message.reply_text(f"{message.from_user.first_name} is now afk!")
+        await message.reply_text("Please reply to a message with an animation or photo, or provide a reason for your afk status.") 
+
 
 async def chat_watcher_func(bot, message):
     if message.sender_chat:
